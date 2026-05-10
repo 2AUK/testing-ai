f4570d7bf3bcfa033b8914984981b232b917f252
Abdullah Ahmad <ahmada1uk@googlemail.com>
2026-05-10 17:10:03 +0100

    local ollama agent can be prompted via tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bytes::BytesMut;
+use rig::agent::Agent;
+use rig::client::{CompletionClient, Nothing};
+use rig::completion::Prompt;
+use rig::providers::ollama::{self, CompletionModel};
 use serde::{Deserialize, Serialize};
+use tokio::io::AsyncWriteExt;
 use tokio::{io::AsyncReadExt, net::TcpListener};
 
 #[derive(Debug, Deserialize)]
@@ -19,19 +24,24 @@ struct AgentResponse {
 }
 
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    let client = ollama::Client::new(Nothing)?;
+
+    let agent = client.agent("lfm2.5-thinking").preamble("You are a research assistant who specialises in system design, systems thinking, and Rust").build();
 
     loop {
-        let (stream, _) = listener.accept().await.unwrap();
-        tokio::spawn(handle_connection(stream));
+        let (stream, _) = listener.accept().await?;
+        handle_connection(stream, &agent).await;
     }
 }
 
-async fn handle_connection(mut stream: tokio::net::TcpStream) {
+async fn handle_connection(mut stream: tokio::net::TcpStream, agent: &Agent<CompletionModel>) {
     let mut buf = BytesMut::with_capacity(1024);
     stream.read_buf(&mut buf).await.unwrap();
     println!("{:?}", buf);
     let request: AgentRequest = serde_json::from_slice(&buf).unwrap();
-    println!("{:?}", request);
+    let response = agent.prompt(request.request.as_str()).await.unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
